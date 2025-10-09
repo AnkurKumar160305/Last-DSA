2eb0e7d2003a2e96910a6e611bcaea759e5b0738
Ankur <142652031+AnkurKumar160305@users.noreply.github.com>
2025-10-09 17:23:12 +0530

    added optimal approach

--- Week1/day4/350-IntersectionofTwoarraysII.cpp
+++ Week1/day4/350-IntersectionofTwoarraysII.cpp
@@ -1,3 +1,31 @@
+// Type	Complexity
+// Time	O(n + m)
+// Space	O(min(n, m))
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        unordered_map<int,int> m;
+        vector<int> ans;
+        for(int i=0;i<n2;i++){
+            m[nums2[i]]++;
+        }
+
+        for(int i=0;i<n1;i++){
+            if(m[nums1[i]]>0){
+                ans.push_back(nums1[i]);
+                m[nums1[i]]--;
+            }
+        }
+
+        return ans;
+
+
+    }
+};
+
+
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
